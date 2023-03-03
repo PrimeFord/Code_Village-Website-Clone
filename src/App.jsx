b7e1891662b939c15d0b952d8e8df1f3ccaf3f5b
PrimeFord <bashfordbash@gmail.com>
2023-03-03 19:05:04 +0100

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -9,99 +9,79 @@ function App() {
   return (
     <div className="App">
       <div className=" wrp1">
-        <div className="log py-[4rem]">
+        <div className="log">
           <img className="logo" src="./image/cv-logo.svg" alt="" />
         </div>
         <div className="talk">
-          <p className="world w-[35%] text-[#18214d] text-[5rem] font-medium leading-[5rem]">
+          <p className="world  text-[#18214d] font-medium leading-[5rem]">
             We build world class Tech talents
           </p>
-          <p className="w-[50%] mt-[1.5rem] text-[#18214d] text-[1.5rem] font-small leading-[2rem]">
+          <p className="wrld text-[#18214d]  font-small">
             Join our next cohort to accelerate your dream of being a World Class
             Software Developer. No coding experience required.
           </p>
-          <button className="bg-green-800 text-white h-[3rem] w-[10rem] mt-[3rem] rounded-md">
+          <button className="btn bg-green-800 text-white  rounded-md">
             Enroll Now
           </button>
         </div>
-        <div className=" flex gap-5 #">
-          <img
-            className="h-[2rem] w-[2rem]"
-            src="/image/fb.svg"
-            alt="facebook"
-          />
-          <img
-            className="h-[2rem] w-[2rem]"
-            src="/image/tw.svg"
-            alt="twitter"
-          />
-          <img
-            className="h-[2rem] w-[2rem]"
-            src="/image/github.svg"
-            alt="github"
-          />
-          <img
-            className="h-[2rem] w-[2rem]"
-            src="/image/location.svg"
-            alt="location"
-          />
+        <div className="social flex">
+          <img className="h" src="/image/fb.svg" alt="facebook" />
+          <img className="h" src="/image/tw.svg" alt="twitter" />
+          <img className="h" src="/image/github.svg" alt="github" />
+          <img className="h" src="/image/location.svg" alt="location" />
         </div>
         <div className="bg absolute left-[29%]">
           <img src="./image/wallpaper.png" alt="" />
         </div>
       </div>
       <div className="wrp2">
-        <div className="w-[100%] flex justify-between">
-          <p className=" w-[50%] text-[#18214d] text-[3rem] font-medium leading-[3rem]">
+        <div className="first w-[100%] flex justify-between">
+          <p className="first1 text-[#18214d] font-medium">
             State of The Art Ecosystem for Creatives
           </p>
-          <p className=" w-[39%] text-[#18214d] text-[1.1rem] font-small leading-[1.8rem]">
+          <p className="first2 text-[#18214d] font-small">
             Be a part of an all-inclusive technology hub where you learn,
             collaborate with tech creatives and join a team of support systems
             offering you the top tools and frameworks to build solutions and
             earn big.
           </p>
         </div>
-        <div className="rounded-2xl w-[100%] h-[100%]">
+        <div className="pic w-[100%] h-[100%]">
           <img
             className="h-[100%] w-[100%]"
             src="/image/cv-office.jpg"
             alt=""
           />
         </div>
-        <div className="w-[100%] h-[10rem] flex text-[#3e4462] justify-between">
-          <div className="h-[100%] flex align-top w-[31.5%]">
+        <div className="schedule w-[100%] flex text-[#3e4462] justify-between">
+          <div className="h-[100%] flex align-top w">
             <div className="w-[17%]">
               <img
-                className="w-[3rem] h-[3rem]"
+                className=" w-[3rem] h-[3rem]"
                 src="/image/bookmark.svg"
                 alt=""
               />
             </div>
             <div className="w-[83%]">
-              <p className="text-[1.75rem] font-small leading-9">
-                Web & Mobile Development
-              </p>
-              <p className="mt-[.5rem] text-[1.1rem] font-small">
+              <p className="te font-small">Web & Mobile Development</p>
+              <p className=" x font-small">
                 We design and develop tech solutions for SMEs and Government
                 Agency.
               </p>
             </div>
           </div>
-          <div className="h-[100%] flex align-top w-[31.5%]">
+          <div className="h-[100%] flex align-top w">
             <div className="w-[17%]">
               <img className="w-[3rem] h-[3rem]" src="/image/tent.svg" alt="" />
             </div>
             <div className="w-[83%]">
-              <p className="text-[1.75rem] font-small leading-9">
-                Bootcamps & Trainings
-              </p>
-              <p className="mt-[.5rem] text-[1.1rem] font-small">
+              <p className="te font-small ">Bootcamps & Trainings</p>
+              <p className=" x font-small">
                 We’re building the next generation of tech talents.
               </p>
             </div>
           </div>
-          <div className="h-[100%] flex align-top w-[31.5%]">
+          <div className="h-[100%] flex align-top w">
             <div className="w-[17%]">
               <img
                 className="w-[3rem] h-[3rem]"
@@ -110,10 +90,8 @@ function App() {
               />
             </div>
             <div className="w-[83%]">
-              <p className="text-[1.75rem] font-small leading-9">
-                Workspaces & Tech Hub
-              </p>
-              <p className="mt-[.5rem] text-[1.1rem] font-small">
+              <p className="te font-small ">Workspaces & Tech Hub</p>
+              <p className=" x font-small">
                 Work and Connect with other Creatives. All in a fun and cozy
                 environment.
               </p>
@@ -122,20 +100,20 @@ function App() {
         </div>
       </div>
       <div className="wrp3">
-        <div className="w-[65%]">
-          <p className=" w-[100%] text-[#18214d] text-[3.25rem] font-medium leading-[3.2rem] 	tracking-[0em]">
+        <div className="sec1">
+          <p className=" w-[100%] text-[#18214d] text-[3.25rem] font-medium	tracking-[0em]">
             Learn Web & Mobile Development. Access the global job market.
           </p>
-          <p className=" w-[100%] text-[#18214d] text-[1.1rem] font-small leading-[1.8rem] pt-5">
+          <p className=" w-[100%] text-[#18214d]  font-small  pt-5">
             The world is constantly in need of technical talent supply pool.
             Tech is needed accross every sector. Your course of study doesn’t
             matter. All you need is the skill and experience.
           </p>
-          <button className="bg-green-800 text-white h-[3.2rem] w-[10.5rem] mt-[3rem] rounded-md">
+          <button className="btn bg-green-800 text-white rounded-md">
             Learn more
           </button>
         </div>
-        <div className="w-[35%] pl-[3.5rem] h-[fit-content] border-l border-gray-500 grid gap-7">
+        <div className="sec2 ">
           <Program
             img="/image/mail.svg"
             header="Program Schedule"
@@ -156,16 +134,16 @@ function App() {
         </div>
       </div>
       <div className="wrp4">
-        <div className="w-[100%] text-center block  ">
-          <p className=" w-[100%] text-[#18214d] text-[3.25rem] leading-[3.2rem] font-bold tracking-[0em]">
+        <div className="w-[100%] text-center ">
+          <p className=" w-[100%] text-[#18214d] text-[3.25rem] leading-[3.2rem] font-bold tracking-[0em] mb-4">
             Our Expertise
           </p>
-          <p className=" w-[60%] text-[#18214d] text-[1.25rem] font-small leading-[1.8rem] pt-5 m-[auto]">
+          <p className="bl  text-[#18214d] font-small  pt-5 m-[auto]">
             We have built several solutions from ground up using the following
             frameworks. Our world class expertise is enviable.
           </p>
         </div>
-        <div className="mt-[3rem] p-[2rem] flex flex-wrap gap-16 justify-center text-center items-center">
+        <div className="lang mt-[3rem]  justify-center text-center items-center">
           <img src="/image/andriod.svg"></img>
           <img src="/image/bootstrap.svg"></img>
           <img src="/image/aws.svg"></img>
@@ -190,7 +168,7 @@ function App() {
         </div>
       </div>
       <div className="wrp5">
-        <p className=" w-[50%] text-[#18214d] text-[3.25rem] leading-[3.2rem] font-medium tracking-[0em] m-[auto] ">
+        <p className="for text-[#18214d]  font-medium tracking-[0em] m-[auto] ">
           Codevillage Kids & Teens Code Classes
         </p>
         <p className=" w-[75%] text-[#18214d] text-[1.2rem] font-small leading-[1.8rem] pt-5 m-[auto] ">
@@ -208,7 +186,7 @@ function App() {
         </div>
       </div>
       <footer className="wrp6">
-        <div className="flex justify-between gap-[5rem] text-[#3e4462]">
+        <div className="abt flex justify-between gap-[5rem] text-[#3e4462]">
           <div className="w-[12rem]  h-fit grid gap-4">
             <p>
               <img
